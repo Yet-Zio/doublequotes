d5d515e1a1b7bf3826081772bfb5e9a14134da07
Mahesh Yet Zio <maheshdeluxe53@gmail.com>
2024-03-31 18:02:26 +0530

    make popularsort comp

--- frontend/src/components/oth/PopularSort.tsx
+++ frontend/src/components/oth/PopularSort.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react"
 import Select from "../layout/Select"
 
 export default function PopularSort() {
-  const [filterObj, setFilterObj] = useState<PopularSort>({type: "Hot", location: "Everywhere", view: "Card"})
 
   return (
-    <div className="flex self-start ms-20 mb-7">
-      <Select type="type"/>
+    <div className="flex flex-col w-full self-start ps-7 mb-7">
+      <div className="flex w-full ">
+        <Select type="type"/>
+        <Select type="location"/>
+        <Select type="view"/>
+      </div>
+      <hr className='border-0 mt-2 h-[1px] md:w-3/4 bg-[#3c3e39]'/>
     </div>
   )
 }
