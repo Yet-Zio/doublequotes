bc1a295cfd4492dabbb5875a2e2530a14437b36c
Mahesh Yet Zio <maheshdeluxe53@gmail.com>
2024-04-17 00:03:13 +0530

    add inputs to login form

--- frontend/src/common/auth/Login.tsx
+++ frontend/src/common/auth/Login.tsx
@@ -1,7 +1,14 @@
-import { useEffect } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import Doodle from "./Doodle";
+import { Eye, EyeSlash } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
+
+  const [passHidden, setPassHidden] = useState(true)
+  const [passFocused, setPassFocused] = useState(false)
+
+  const [loginDetails, setLoginDetails] = useState({id: "", password: ""})
 
   const handleGoogleSigninResponse = (response: any) => {
     console.log("Encoded JWT:", response.credential)
@@ -13,7 +20,6 @@ export default function Login() {
       callback: handleGoogleSigninResponse
     })
 
-    // @ts-ignore
     google.accounts.id.renderButton(
       document.getElementById('gSignInDiv')!,
       {
@@ -26,30 +32,28 @@ export default function Login() {
     document.body.style.overflow = 'hidden';
 
     initGoogleSignIn()
+    setInterval(genDoodle, 5000)
 
     return () => {
       document.body.style.overflow = 'unset';
     };
   }, []); 
 
-  const handleBGClick = () => {
-    clearInterval(interval)
-    genDoodle()
-    interval = setInterval(genDoodle, 2000)
-  }
-
   const genDoodle = () => {
     document.querySelectorAll('css-doodle').forEach(function (o: any) {o.update()})
   }
 
-  var interval = setInterval(genDoodle, 2000)
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    console.log("form event handled")
+  }
 
   return (
     <div className="flex min-w-screen min-h-screen justify-center items-center">
-      <div className="flex w-full h-full select-none" onClick={() => handleBGClick()}>
+      <div className="flex w-full h-full select-none">
         <Doodle/>
       </div>
-      <div className="absolute flex flex-col w-[530px] h-[644px] bg-[#1a1e15] rounded-2xl useinter p-10">
+      <form onSubmit={handleSubmit} className="absolute flex flex-col w-[530px] h-[644px] bg-[#1a1e15] rounded-2xl useinter p-10">
           <span className="text-2xl font-extrabold">Log In</span>
           <span className="mt-2 text-xs text-gray-300">By continuing, you can use Double Quotes to its full extent. Your data is not used for any other purposes as this is a hobby project.</span>
           <div id="gSignInDiv" className="mt-3 rounded-full overflow-hidden self-center"></div>
@@ -58,9 +62,18 @@ export default function Login() {
             <span className="text-[#6a765b] text-sm">OR</span>
             <div className='flex border-0 h-[1px] w-full bg-[#3e4535] ms-2'></div>
           </div>
-          <input type="text" className="mt-5 ms-5 me-5 p-4 ps-5 pe-5 rounded-3xl outline-0 bg-zinc-700 text-gray-400 placeholder-gray-400 hover:bg-zinc-700/75" placeholder="Email or username *"/>
-          <input type="password" className="mt-5 ms-5 me-5 p-4 ps-5 pe-5 rounded-3xl outline-0 bg-zinc-700 text-gray-400 placeholder-gray-400 hover:bg-zinc-700/75" placeholder="Password *"/>
-        </div>
+          <input value={loginDetails.id} onChange={(e) => {setLoginDetails({...loginDetails, id: e.target.value})}} type="text" className="mt-5 ms-5 me-5 p-4 ps-5 pe-5 rounded-3xl outline-0 bg-[#3e4535]/25 text-gray-400 placeholder-gray-400 hover:bg-[#3e4535]/50 focus:bg-[#3e4535]/50" placeholder="Email or username *" required/>
+          <div className={`flex mt-5 ms-5 me-5 rounded-3xl items-center  text-gray-400 placeholder-gray-400 hover:bg-[#3e4535]/50 ${passFocused ? "bg-[#3e4535]/50" : "bg-[#3e4535]/25"}`}>
+            <input value={loginDetails.password} onChange={(e) => {setLoginDetails({...loginDetails, password: e.target.value})}} type={passHidden ? "password" : "text"} className="bg-transparent w-full h-full p-4 ps-5 pe-5 outline-0 border-0" placeholder="Password *" required onFocus={() => setPassFocused(!passFocused)} onBlur={() => setPassFocused(!passFocused)}/>
+            {passHidden ? <EyeSlash size={24} onClick={() => setPassHidden(!passHidden)} className="me-5"/> : 
+              <Eye size={24} onClick={() => setPassHidden(!passHidden)} className="me-5"/>}
+          </div>
+          <Link to={"/forgotpass"} className="mt-5 ms-5 text-sm text-[#A9A74F] w-[120px]">Forgot Password?</Link>
+          <span className="mt-5 ms-5 text-sm">New to DoubleQuotes?&nbsp;<span className="text-[#A9A74F] cursor-pointer">Sign Up</span></span>
+          <div className="flex flex-col h-40 justify-end">
+            <button disabled={loginDetails.id === "" && loginDetails.password === ""} className={`mt-2 p-3 ps-7 pe-7 rounded-full font-semibold ${loginDetails.id !== "" && loginDetails.password !== "" ? "bg-[#A9A74F] hover:bg-[#A9A74F]/75" : "bg-[#3e4535]/10 text-[#3e4535]/45"}`}>Login</button>
+          </div>
+      </form>
     </div>
   );
 }
