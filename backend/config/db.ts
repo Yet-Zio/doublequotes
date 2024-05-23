c6a8ac824e9638c0bfbcf2b030992ec4cd830993
Mahesh Yet Zio <maheshdeluxe53@gmail.com>
2024-05-23 19:36:37 +0530

    add backend docker support

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectToMongo = async () => {
-    const mongoURI: string  = process.env.MONGOURI!
+    const mongoURI: string  = (process.env.IN_DOCKER === "true" ? process.env.DOCKER_MONGOURI : process.env.MONGOURI) as string
+    
     mongoose.connect(mongoURI)
         .then(success => {
             console.log("Connected to MongoDB successfully!\nDB:", success.connection.name)
--- backend/index.ts
+++ backend/index.ts
@@ -11,25 +11,29 @@ import { authRouter } from "./routes/authRoute";
 import { verifyRouter } from "./routes/verifyRoute"
 import { serverErrorHandler } from "./utils/errorHandler";
 
-const app: Express = express()
-const port: string | number = process.env.PORT || 3000
+async function startServer(){
+    const app: Express = express()
+    const port: string | number = process.env.PORT || 3000
 
-app.use(serverCors)
-app.use(express.json())
-app.use(cookieParser())
-app.use(compression())
+    app.use(serverCors)
+    app.use(express.json())
+    app.use(cookieParser())
+    app.use(compression())
 
-connectToMongo()
+    await connectToMongo()
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("(⓿_⓿): Hello, you are communicating with DoubleQuotes.")
-})
+    app.get("/", (req: Request, res: Response) => {
+        res.send("(⓿_⓿): Hello, you are communicating with DoubleQuotes.")
+    })
 
-app.use("/api/auth", authRouter)
-app.use(verifyRouter)
+    app.use("/api/auth", authRouter)
+    app.use(verifyRouter)
 
-app.use(serverErrorHandler)
+    app.use(serverErrorHandler)
 
-app.listen(port, () => {
-    console.log(`[server]: Server has started running on http://localhost:${port}`)
-})
+    app.listen(port, () => {
+        console.log(`[server]: Server has started running on http://localhost:${port}`)
+    })
+}
+
+startServer()
